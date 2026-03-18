fbfb415c5438d1a7f28487c880df339b173b568d
JoaoPedroSaid <jottapp090@gmail.com>
2026-03-18 17:35:28 -0300

    Create questoes.java

---
+++ questoes.java
@@ -0,0 +1,38 @@
+public class questoes.java {
+
+    public static void main(String[] args){
+        
+/*  01: for (int a = 0; a<=10; a++){
+        System.out.println(a);
+    }
+*/
+
+/*  02: for (int a = 0; a<=100; a=a+2){
+        System.out.println(a);
+    }
+*/      
+    
+     int numero = 67;
+     
+    for (int a=0; a<=10; a++){
+        System.out.println(numero*a);
+     }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
